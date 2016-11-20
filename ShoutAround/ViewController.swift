4d01807a3b34e131eea97fa72de44a6313992986
planert41 <planert41@gmail.com>
2016-11-20 11:25:53 -0600

    Initial Commit

---
+++ ShoutAround/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ShoutAround
+//
+//  Created by Wei Zou Ang on 11/20/16.
+//  Copyright © 2016 Wei Zou Ang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
